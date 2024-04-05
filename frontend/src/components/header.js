3e16c4941e3996bf59ad9dc27cc368457d29b235
karthik-pv <karthik.pv77@gmail.com>
2024-04-05 21:56:44 +0530

    removed typist

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom'
-import Typist from "react-typist";
 
 const Header = () => {
     const navigate = useNavigate()
@@ -11,9 +10,7 @@ const Header = () => {
 
     return (    
         <div className="pt-10 pb-10 text-center">
-            <Typist cursor={{show:false}}>
-                <h1 className="text-6xl text-white cursor-pointer" onClick={goToHome}>Emuser</h1>
-            </Typist>
+            <h1 className="text-6xl text-white cursor-pointer" onClick={goToHome}>Emuser</h1>
         </div>
     )
 }
