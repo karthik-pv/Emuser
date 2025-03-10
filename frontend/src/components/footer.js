a5c1b4fcc01f46ee4d430e9f77554312c4d47fad
Karthik PV <106957053+karthik-pv@users.noreply.github.com>
2025-03-11 00:12:29 +0530

    Update footer.js

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return(
-        <div className="pt-5">
+        <div className="pt-5 text-white">
             Developed by <a className="text-white underline" href="https://github.com/karthik-pv" target="_blank">Jojo</a> and <a className="text-white underline" href="https://github.com/aishwarya-ad" target="_blank">Aishh</a>
         </div>
     )
